baa30215b3e0d278f0c1812da8f1896f54b8d84b
chenged118 <chengoldshan@gmail.com>
2025-07-12 20:06:13 +0800

    feat(model): add Order model and initial migration

--- api/models.py
+++ api/models.py
@@ -4,5 +4,9 @@ from django.db import models
 # Create your models here.
 class Order(models.Model):
     # Add your model here
-    pass
+    order_number = models.CharField(max_length=100, unique=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_time = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.order_number
